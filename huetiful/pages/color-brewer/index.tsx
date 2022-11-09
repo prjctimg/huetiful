f057b66cd43deb7b4c11e34bde20e54229191530
prjctimg <prjctimg@outlook.com>
2022-11-09 07:28:13 +0200

    Fixed color-brewer scales layout. Maybe convert them to Accordions??.[MUST] Work on the typography and overall layout

--- huetiful/pages/color-brewer/index.tsx
+++ huetiful/pages/color-brewer/index.tsx
@@ -1,9 +1,9 @@
-import { Paper, Typography } from '@mui/material';
+import { Card, CardContent, Grid, Paper, Typography } from '@mui/material';
 import { Stack } from '@mui/system';
 import _ from 'lodash';
 import { colorbrewer } from '../../app/helpers/colorBrewer.mjs';
-export default function ColorBrewer() {
-    return (
+
+/* 
         <>
             {_.map(colorbrewer, (brew, name) => {
                 return (
@@ -18,5 +18,36 @@ export default function ColorBrewer() {
                 );
             })}
         </>
+    );
+}
+ */
+
+export default function ColorBrewer() {
+    return (
+        <Grid rowSpacing={2} container>
+            {_.map(colorbrewer, (brew, name) => {
+                return (
+                    <Grid xs={12} sm={8} item>
+                        <Card>
+                            <CardContent>
+                                <Typography>{name}</Typography>
+                                <Stack direction="row" columnSpacing={3} spacing={2}>
+                                    {_.map(brew, (hex) => {
+                                        return (
+                                            <Paper
+                                                className="w-[40px] h-[40px]"
+                                                style={{
+                                                    background: hex
+                                                }}
+                                            />
+                                        );
+                                    })}
+                                </Stack>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                );
+            })}
+        </Grid>
     );
 }
--- huetiful/pages/gradients/index.tsx
+++ huetiful/pages/gradients/index.tsx
@@ -1,40 +1,33 @@
-
-import { Paper, Grid, Typography, Card, CardActions, Input, Select } from '@mui/material'
-import { gradients } from '../../app/codelab/gradients.mjs'
-import { grey } from '@mui/material/colors'
-import { PaperStack } from '../../components/paper-stack.jsx'
-
-
+import { Paper, Grid, Typography, Card, CardActions, Input, Select } from '@mui/material';
+import { gradients } from '../../app/codelab/gradients.mjs';
+import { grey } from '@mui/material/colors';
+import { PaperStack } from '../../components/paper-stack.jsx';
 
 function GradientsPage() {
     return (
         <>
             <title>Gradient generator</title>
             <Grid container>
+                <Typography variant="h5" fontWeight="light">
+                    Gradient generator
+                </Typography>
 
-                <Typography variant='h5' fontWeight='light'>Gradient generator</Typography>
-                <Grid item xs={12} >
-                    <Card  >
-                        { /* Card will have to output the select values live. */}
-                        <Paper className='w-full   bg-cyan-600 rounded-md xs:h-[50vh]' />
+                <Grid item xs={12}>
+                    <Card>
+                        {/* Card will have to output the select values live. */}
+                        <Paper className="w-full   bg-cyan-600 rounded-md xs:h-[50vh]" />
 
                         <Grid container direction={'row'} spacing={2}>
                             <Grid item>
-                                <Paper className='w-8 h-8 bg-black' />
+                                <Paper className="w-8 h-8 bg-black" />
                             </Grid>
-
                         </Grid>
-                        <Input type='color' className='w-6 h-6 rounded-md' />
-
-
-
+                        <Input type="color" className="w-6 h-6 rounded-md" />
                     </Card>
-
                 </Grid>
             </Grid>
         </>
-    )
+    );
 }
 
-
-export default GradientsPage
+export default GradientsPage;
