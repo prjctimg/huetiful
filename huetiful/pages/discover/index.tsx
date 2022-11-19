d7b3a4d3d8859544eb2c783c112047aa7c08e6db
prjctimg <prjctimg@outlook.com>
2022-11-19 13:06:40 +0200

    Removedundefined component references in the discover section.

--- huetiful/pages/discover/index.tsx
+++ huetiful/pages/discover/index.tsx
@@ -1,9 +1,5 @@
-import PaperStack from '../../components/paper-stack'
+import PaperStack from '../../components/paper-stack';
 
 export default function DiscoverPalettes() {
-
-    return (
-        <PaperStack color={ } />
-    )
+    return <h1>Hueshifted palettes</h1>;
 }
-
