1f98f85f21d61a4b23405d18e3233f02d0e633ae
prjctimg <prjctimg@outlook.com>
2022-11-19 12:33:30 +0200

    .

--- huetiful/pages/_app.tsx
+++ huetiful/pages/_app.tsx
@@ -1,23 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Layout from '../components/layout'
-
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-
-
-
-
-
-
-    </>
-  )
+    return (
+        <>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </>
+    );
 }
 
-export default MyApp
-
+export default MyApp;
