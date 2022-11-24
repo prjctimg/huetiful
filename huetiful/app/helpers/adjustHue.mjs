3387fb838c5725f65dba110e1c6ebed02d3d237b
prjctimg <prjctimg@outlook.com>
2022-11-24 11:07:50 +0200

    Made a small helper function to convert the LCH color objects to hex codes for browser compatibity.

--- huetiful/app/helpers/adjustHue.mjs
+++ huetiful/app/helpers/adjustHue.mjs
@@ -1,9 +1,12 @@
+
+
 //Small helper function to correct hue values and clamp them to the 360 degrees limit
 
 
-export function adjustHue(value) {
+export default function adjustHue(value = 0) {
     if (value < 0)
         value += Math.ceil(-value / 360) * 360;
 
     return value % 360;
 }
+
--- huetiful/app/utils/createPalettes.mjs
+++ huetiful/app/utils/createPalettes.mjs
@@ -1,11 +1,8 @@
+import adjustHue from '../helpers/adjustHue.mjs'
+import _ from 'lodash'
+import chroma from 'chroma-js'
 
-function adjustHue(val) {
-  if (val < 0) val += Math.ceil(-val / 360) * 360;
-
-  return val % 360;
-}
-
-function createScientificPalettes(baseColor) {
+function createPalettes(baseColor = { l: 0, c: 0, h: 0 }) {
   const targetHueSteps = {
     analogous: [0, 30, 60],
     triadic: [0, 120, 240],
@@ -28,6 +25,10 @@ function createScientificPalettes(baseColor) {
   return palettes;
 }
 
+
+
+
+
 function generate() {
   // choose a random base color
   const base = {
@@ -36,4 +37,23 @@ function generate() {
     h: Math.random() * 360,
     mode: "lch"
   };
+}
+const d = createPalettes({ l: 70, c: 80, h: 70 })
+
+
+//MUST make the function more generic
+/**
+ * Takes the palette object and returns the colorspace converted to hex. 
+ * @param {} lchPaletteObj 
+ */
+export function lch2hex(lchPaletteObj = {}) {
+  _.forEach(lchPaletteObj, (value, key) => {
+
+    return value.map(lchObj => chroma.lch(lchObj).hex('lch'))
+  })
+
+}
+
+module.exports = {
+  lch2hex, createScientificPalettes: createPalettes
 }
