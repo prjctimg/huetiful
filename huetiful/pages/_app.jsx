0f407ce150b75220fe7e7c2b86fb9b8820dd32b0
prjctimg <prjctimg@outlook.com>
2022-11-25 05:50:10 +0200

    Changed the whole codebase to jsx extension.

--- huetiful/pages/_app.jsx
+++ huetiful/pages/_app.jsx
@@ -1,8 +1,7 @@
 import '../styles/globals.css';
-import type { AppProps } from 'next/app';
 import Layout from '../components/layout';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }) {
     return (
         <>
             <Layout>
@@ -12,4 +11,4 @@ function MyApp({ Component, pageProps }: AppProps) {
     );
 }
 
-export default MyApp;
+export default App;
