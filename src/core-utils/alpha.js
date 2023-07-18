851b6bd4f16d582a3a8b4bd04037ec839149a30d
prjctimg <prjctimg@outlook.com>
2023-07-18 12:05:32 +0200

    alpha.js now documented and ready for testing/review

--- src/core-utils/alpha.js
+++ src/core-utils/alpha.js
@@ -1,23 +1,29 @@
-import { converter } from "culori";
-import { isNumber } from "lodash-es";
+import { converter, formatHex8 } from "culori";
+import { defaultTo, divide, inRange, isNumber, isUndefined } from "lodash-es";
 
 //@ts-check
 
 /**
- * Sets/gets the opacity of a color.
+ * Sets/gets the opacity of a color. The value is between [0,1]
  *
- * @param color
- * @param value
- * @returns
+ * @param color The color with the targeted opacity channel. Takes any valid color token.
+ * @param value The value to apply to the opacity channel.
+ * @returns {string} color The resulting color. Returns an 8 character hex code.
  */
 
 const alpha = (color, value) => {
-  const src = converter("rgb")(color);
+  // We never perfom an operation on an undefined color. Defaults to pure black
+  defaultTo(color, "#000000");
+  if (isUndefined(value)) {
+    return formatHex8(color);
+  } else if (isNumber(value)) {
+    inRange(value, 0, 1.0) ? alpha : divide(alpha, 100);
+    const src = converter("rgb")(color);
 
-  if (alpha !== undefined && isNumber(alpha)) {
-    src["alpha"] = alpha;
+    src["alpha"] = value;
+
+    return formatHex8(src);
   }
-  return src;
 };
 
 export { alpha };
