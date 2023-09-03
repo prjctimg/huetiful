94866f524db5f018a9b08191677fadc075555132
prjctimg <prjctimg@outlook.com>
2023-09-03 22:06:45 +0200

    Added hue.ts

--- src/colors/hue.ts
+++ src/colors/hue.ts
@@ -1,19 +1,43 @@
 //This module contains getNearestHue,getFarthestHue,minHue and maxHue which are collection based utils that return the color with the queried factor.
 
-import { converter } from "culori";
 import { Color, factor } from "../paramTypes";
 import { getChannel } from "../core-utils/get.ts";
+import { fromPairs, get, head, map, subtract, tail } from "lodash-es";
+import { colorObjArr, sortedArr } from "../core-utils/helpers";
 
-const getNearestHue = (color: Color, colors: Color[]) => {
-  const factor: factor = "hue";
-  const cb = getChannel("lch.h");
+//  Globals
 
-  // First store the hue value of the color being used as a comparison.
-  const targetHue = cb(color);
-};
-const getFarthestHue = (first) => {
-  second;
-};
+const { abs } = Math;
+const factor: factor = "hue";
+const mode = "lch.h";
+// The hue value of our color which we are using for comparison
+const targetHue = (color: Color): number => getChannel(mode)(color);
+
+// The callback to invoke per color in the passed in collection.
+// Return the absolute value since hue is a cyclic value which can either be  in clockwise/anti-clockwise.
+//This means that the color object with the smallest hue value is the  nearest color/hue.
+const cb = (color: Color) => (subtrahend: Color) =>
+  abs(subtract(targetHue(color), getChannel(mode)(subtrahend)));
+
+/**
+ *@function
+ @description
+ * @param color
+ * @param colors
+ * @returns
+ */
+const getNearestHue = (color: Color, colors: Color[]): number =>
+  get(tail(sortedArr(factor, cb(color))(colors)), factor);
+
+/**
+ *@function
+ @description
+ * @param color
+ * @param colors
+ * @returns
+ */
+const getFarthestHue = (color: Color, colors: Color[]): number =>
+  get(head(sortedArr(factor, cb(color))(colors)), factor);
 const minHue = (first) => {
   second;
 };
