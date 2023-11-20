3232b7dff8c1a8c4907865cc5900abd7f5c8c5f7
Dean Tarisai <prjctimg@outlook.com>
2023-11-20 12:21:37 +0200

    Fixed import errors in package.json

--- src/palettes/adaptive.ts
+++ src/palettes/adaptive.ts
@@ -1,3 +1,24 @@
 // @ts-nocheck
 
 // This module will make use of contrast ratio to create adaptive palettes
+
+// Things I need to understand first:
+// 1. What is adaptive color exactly
+// 2. What makes color adaptive
+// 3. What is contrast and how does it affect adaptive color
+// 4. What are the specifics of contrast ratio in relation to design elements
+// 5. What are the nuances of dark/light mode. How is the color corrected when we switch themes
+// 6. How can this be implemented in code
+
+// adaptive returns an object of the adjusted colors
+// {lightMode/darkMode:Color[]}
+
+// Possible params
+// 1. backgroundColor
+// 2. foregroundColor
+// Do I need constrains for the background
+
+const lightMode = () => {};
+const darkMode = () => {};
+
+export { lightMode, darkMode };
