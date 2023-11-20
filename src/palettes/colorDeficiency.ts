7208a55e891e3071eda0e4fe81bef6cbd8bee909
Dean Tarisai <prjctimg@outlook.com>
2023-11-20 18:34:11 +0200

    Finished brainstorming the adaptive and colorDeficiency utils. Now awaiting prototyping.

---
+++ src/palettes/colorDeficiency.ts
@@ -0,0 +1,10 @@
+// @ts-nocheck
+
+// This module is focused on creating color blind safe palettes that adhere to the minimum contrast requirements
+
+// How can I achieve this ?
+// 1. First I pass the color(s) to a color vision deficiency simulation function
+// 2. Check if the color has the minimum required contrast as compared to a dark/light mode surface which can optionally be overriden
+// Check the min luminance contrast ratio between the color and background.
+// So which channels do I need to tweak in order to fix up the colors.
+// Maybe provide an optional adaptive boolean which returns dark/light mode variant colors of the color blind safe palettes.
