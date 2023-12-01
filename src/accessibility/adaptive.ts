14c69c858abfc8e037d4f672d350adab100a05b5
Dean Tarisai <prjctimg@outlook.com>
2023-12-01 23:58:58 +0200

    Found some info on how to implement adaptive palette

--- src/accessibility/adaptive.ts
+++ src/accessibility/adaptive.ts
@@ -23,16 +23,37 @@
 // Do I need constrains for the background
 
 // First I need to declare some constants to serve as starting points
-const MIN_CONTRAST = 0;
-const MAX_CONTRAST = 1;
-const MIN_LUMINANCE = 1;
-const MAX_LUMINANCE = 2;
 
-// Allows us to generate color based based on the desired contrast ratio
-const TARGET_CONTRAST_RATIO = 1;
+const polynomial = (x: number) => {
+  return Math.sqrt(Math.sqrt((Math.pow(x, 2.25) + Math.pow(x, 4)) / 2));
+};
 
+////// Adapted from Adobe color
 
-const lightMode = () => {};
-const darkMode = () => {};
+// Convert the color to an optimized color space for creating background colors
+//
 
-export { lightMode, darkMode };
+// 1. I need a properly defined color object that has all the color properties I'll need for comparisons.
+
+// 2. Smooth scale function
+// - Points = A nested array of each colors channel value [[],[],[]]
+// -
+
+/////// Rough concept of adaptive color
+
+// Dark theme factors to consider
+// 1. if color is darker than background. It means that color is not legible and will be tuned up
+// 2. if color is lighter than background. it means its legible
+
+// Light theme factors to consider
+// 1. if a color is lighter than background. This means color is not compliant and will be fixed up.
+// 2. if a color is darker than background. It means its legible
+
+// Constants
+// - baseluminance
+// - colorluminance
+
+// Find contrast colors
+// find contrast color pairs
+
+// The relative luminance returned should be compliant to the defined ratio
