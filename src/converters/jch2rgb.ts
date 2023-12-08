24784eee251e7d19b8d9111717ef476921bde33c
Dean Tarisai <prjctimg@outlook.com>
2023-12-08 10:21:52 +0200

    Removed redundant jch utilities

--- src/converters/jch2rgb.ts
+++
@@ -1,5 +0,0 @@
-import { xyz, Vector3D } from 'ciebase-ts';
-import { baseCieCam } from './ciecam';
-
-const jch2rgb = (jch: Vector3D) =>
-  xyz(baseCieCam.fromXyz({ J: jch[0], C: jch[1], h: jch[2] })).toRgb(jch);
