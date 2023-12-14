59e4cbe04c283e3fa1d80f742cdf931a002c9b23
Dean Tarisai <prjctimg@outlook.com>
2023-12-14 15:26:05 +0200

    Added jsdoc to the getContrast util

--- source/getters_and_setters/contrast.ts
+++ source/getters_and_setters/contrast.ts
@@ -3,6 +3,19 @@
 import { wcagContrast } from "culori/fn";
 import type { Color } from "../types";
 
+/**
+ * @function
+ * @description Gets the relative luminance of the lightest color
+ * @param color
+ * @param against
+ * @returns The relative luminance of the lightest color.
+ * @example
+ *
+ * import { getContrast } from 'huetiful-js'
+ *
+ * console.log(getContrast("black", "white"));
+ * // 21
+ */
 const getContrast = (color: Color, against: Color): number =>
   wcagContrast(color, against);
 
