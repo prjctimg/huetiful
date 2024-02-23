0cae1e9ddfa552bfb3f4eb78da286574de7ecaa8
Dean Tarisai <prjctimg@outlook.com>
2024-02-23 12:58:36 +0200

    Light mode design for the docs is complete

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -2,7 +2,7 @@
  * Script for the huetiful-js docs site. Contains JS powered perks
  */
 
-// Reading progress
+/** Reading progress */
 
 var [progressBar, body, html] = [
   document.getElementById('progress-bar'),
@@ -26,3 +26,30 @@ function setProgress() {
 window.addEventListener('scroll', setProgress);
 
 setProgress();
+
+/* Dark/light mode toggle */
+
+var lightSwitches = document.querySelectorAll('.light-switch');
+
+if (lightSwitches.length > 0) {
+  lightSwitches.forEach((lightSwitch, idx) => {
+    if (localStorage.getItem('dark-mode') === true) {
+      lightSwitch.checked = true;
+    }
+    lightSwitch.addEventListener('change', () => {
+      let { checked } = lightSwitch;
+      lightSwitches.forEach((el, n) => {
+        if (n !== idx) {
+          el.checked = checked;
+        }
+      });
+      if (lightSwitch.checked) {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('dark-mode', true);
+      } else {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('dark-mode', false);
+      }
+    });
+  });
+}
