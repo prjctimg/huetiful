b7fc63c77eb5fb34532d2e877ba9bd664b7b3a7d
Dean Tarisai <prjctimg@outlook.com>
2024-02-29 10:38:17 +0200

    Tweaked the page layout by moving edit button to the right

--- docs/assets/js/xml-shards/post.cjs
+++ docs/assets/js/xml-shards/post.cjs
@@ -34,7 +34,7 @@ function postFragment({
   references = []
 }) {
   return `
-    <title>${title}</title>
+    <title class='first-letter:uppercase'>${title}</title>
    
      
       
@@ -42,14 +42,14 @@ function postFragment({
     <article class='markdown-body'>
     <div class="flex justify-start w-full space-x-2"> 
 
-<div class='flex flex-col'>
+<div class='flex flex-row w-full justify-end'>
 
 
-<a href=${declFile} class="p-2.5 bg-blue-300  rounded-md shadow-sm"> Edit this page <span class="inline-block ml-1"><span> <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
+<a href=${declFile} ><button class="p-2.5 bg-blue-400  rounded-md shadow-sm"></button> Edit this page <span class="inline-block ml-1"><span> <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
 
     </span></a>
 
-<div class='block text-slate-600 text-xs font-mono font-semibold'> <span class='font-normal text-sm text-slate-500'>last updated</span> ${lastUpdated}</div>
+<div class='block text-slate-600 text-xs font-sans font-semibold'> <span class='font-normal text-sm text-slate-500'>last updated</span class='font-normal'> ${lastUpdated}</div>
 
 </div>
 
@@ -79,13 +79,13 @@ ${mainContent}
 <div class="flex w-full justify-between px-2 py-2">
 
 
-<a href=${page.previous.href}>
+<a href=${page.previous.href} class='inline-flex space-x-2 p-4'>
   <button><svg xmlns="http://www.w3.org/2000/svg" width="22" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left"><polyline points="15 18 9 12 15 6"></polyline></svg><span>Previous page</span></button>
   <h5>${page.previous.title}</h5>
 </a>
 <div class='divide-y-1 divide-slate-700 ' />
-<a href=${page.next.href}>
- <button>Next page</button>
+<a href=${page.next.href} class='inline-flex space-x-2 p-4'>
+ <button>Next page <spa><svg xmlns="http://www.w3.org/2000/svg" width="22" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg></span></button>
  <h5>${page.previous.title}</h5>
 </a>
 
