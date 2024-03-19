bc545b2fb983a6e82239cb547f3c62b656eb9e53
Dean Tarisai <prjctimg@outlook.com>
2024-03-19 13:17:09 +0200

    Updated color.d.ts file

--- types/colors.d.ts
+++ types/colors.d.ts
@@ -1573,7 +1573,55 @@ console.log(color("#a1bd2f").scheme("triadic"))
  *
  */
 declare function color(color: ColorToken): Color;
+
+/**
+ * Returns the nearest color(s) in a collection against
+ * @param collection The collection of colors to search for nearest colors
+ * @param against The color to use for distance comparison.
+ * @param num The number of colors to return, if the value is above the colors in the available sample, the entire collection is returned with colors ordered in ascending order using the `differenceHyab` metric.
+ * @returns An cllection of colors.
+ * @example
+ *
+ * let cols = colors('all', '500')
+ *
+console.log(getNearestColor(cols, 'blue', 3));
+ // [ '#a855f7', '#8b5cf6', '#d946ef' ]
+ */
+declare function getNearestColor(
+  collection: ColorToken[] | 'tailwind',
+  against: ColorToken,
+  num?: number
+): ColorToken | Array<ColorToken> | Map<any, ColorToken>;
+
+/**
+ *
+ * Returns the color as a simulation of the passed in `defeciencyType` of color vision deficiency with the deficiency filter's intensity determined by the `severity` value.
+ * @param deficiencyType The type of color vision deficiency. To avoid writing the long types, the expected parameters are simply the colors that are hard to perceive for the type of color blindness. For example those with 'tritanopia' are unable to perceive 'blue' light. Default is `'red'` when the defeciency parameter is undefined or any falsy value.
+ * @param color The color to return its simulated variant.
+ * @param severity The intensity of the filter. The exepected value is between [0,1]. For example 0.5
+ * @returns The color as its simulated variant. Preserves the `ColorToken` type of the pased in color.
+ * @example
+ *
+ * import { colorDeficiency, color2hex } from 'huetiful-js'
+
+// Here we are simulating color blindness of tritanomaly or we can't see 'blue'.
+// We are passing in our color as an array of channel values in the mode "rgb". The severity is set to 0.1
+let tritanomaly = colorDeficiency('blue')
+console.log(tritanomaly(['rgb', 230, 100, 50, 0.5], 0.1))
+// #dd663680
+
+// Here we are simulating color blindness of tritanomaly or we can't see 'red'. The severity is not explicitly set so it defaults to 1
+let protanopia = colorDeficiency('red')
+console.log(protanopia({ h: 20, w: 50, b: 30, mode: 'hwb' }))
+// #9f9f9f
+ */
+declare function colorDeficiency(
+  deficiencyType?: DeficiencyType
+): (color: ColorToken, severity?: number) => string;
+
 export {
+  colorDeficiency,
+  getNearestColor,
   diverging,
   qualitative,
   sequential,
--- types/utils.d.ts
+++ types/utils.d.ts
@@ -230,62 +230,7 @@ console.log(overtone("blue"))
 // false
  */
 declare function overtone(color: ColorToken): string | boolean;
-/**
- *
- * Gets the smallest lightness value from the passed in colors.
- * @param collection The collection of colors to query the color with the smallest lightness value.
- * @param colorObj Optional boolean that makes the function return a custom object with factor (`lightness`) and name of the color as keys. Default is `false`.
- * @param mode THe mode colorspace to retrieve the lightness value from.
- * @returns The smallest lightness value in the colors passed in or a custom object with the `factor` and the color's `name` as keys.
- * @example
- *
- * import { getNearestLightness } from 'huetiful-js'
 
-let sample = ["b2c3f1", "#a1bd2f", "#f3bac1"]
-
-console.log(getNearestLightness(sample, 'lch',true))
-
-// { lightness: 72.61647882089876, name: '#a1bd2f' }
-
- */
-declare function getNearestLightness(
-  collection: ArrayLike<ColorToken> | object | Map<any, ColorToken>,
-  colorspace?: HueColorSpaces,
-  colorObj?: boolean
-):
-  | number
-  | {
-      factor: number;
-      color: ColorToken;
-    };
-/**
- *
- *  Gets the largest lightness value from the passed in colors.
- * @param collection The collection of colors to query the color with the largest lightness value.
- * @param colorObj Optional boolean that makes the function return a custom object with factor (`lightness`) and name of the color as keys. Default is `false`.
- * @param colorspace THe mode colorspace to retrieve the lightness value from.
- * @returns The largest lightness value in the colors passed in or a custom object with the `factor` and the color's `name` as keys.
- * @example
- *
- * import { getFarthestLightness } from 'huetiful-js'
-
-let sample = ["b2c3f1", "#a1bd2f", "#f3bac1"]
-
-console.log(getFarthestLightness(sample, 'lch',true))
-
-// { lightness: 80.94668903360088, name: '#f3bac1' }
-
- */
-declare function getFarthestLightness(
-  collection: ArrayLike<ColorToken> | object | Map<any, ColorToken>,
-  colorspace?: HueColorSpaces,
-  colorObj?: boolean
-):
-  | number
-  | {
-      factor: number;
-      color: ColorToken;
-    };
 /**
  *
  * Darkens the color by reducing the lightness channel. .
@@ -376,87 +321,20 @@ declare function isAchromatic(
   color: ColorToken,
   colorspace?: HueColorSpaces
 ): boolean;
-/**
- *
- * Returns the color as a simulation of the passed in `defeciencyType` of color vision deficiency with the deficiency filter's intensity determined by the `severity` value.
- * @param deficiencyType The type of color vision deficiency. To avoid writing the long types, the expected parameters are simply the colors that are hard to perceive for the type of color blindness. For example those with 'tritanopia' are unable to perceive 'blue' light. Default is `'red'` when the defeciency parameter is undefined or any falsy value.
- * @param color The color to return its simulated variant.
- * @param severity The intensity of the filter. The exepected value is between [0,1]. For example 0.5
- * @returns The color as its simulated variant. Preserves the `ColorToken` type of the pased in color.
- * @example
- *
- * import { colorDeficiency, color2hex } from 'huetiful-js'
-
-// Here we are simulating color blindness of tritanomaly or we can't see 'blue'.
-// We are passing in our color as an array of channel values in the mode "rgb". The severity is set to 0.1
-let tritanomaly = colorDeficiency('blue')
-console.log(tritanomaly(['rgb', 230, 100, 50, 0.5], 0.1))
-// #dd663680
-
-// Here we are simulating color blindness of tritanomaly or we can't see 'red'. The severity is not explicitly set so it defaults to 1
-let protanopia = colorDeficiency('red')
-console.log(protanopia({ h: 20, w: 50, b: 30, mode: 'hwb' }))
-// #9f9f9f
- */
-declare function colorDeficiency(
-  deficiencyType?: DeficiencyType
-): (color: ColorToken, severity?: number) => string;
-/**
- * Returns the nearest color(s) in a collection against
- * @param collection The collection of colors to search for nearest colors
- * @param against The color to use for distance comparison.
- * @param num The number of colors to return, if the value is above the colors in the available sample, the entire collection is returned with colors ordered in ascending order using the `differenceHyab` metric.
- * @returns An cllection of colors.
- * @example
- *
- * let cols = colors('all', '500')
- *
-console.log(getNearestColor(cols, 'blue', 3));
- // [ '#a855f7', '#8b5cf6', '#d946ef' ]
- */
-declare function getNearestColor(
-  collection: ColorToken[] | 'tailwind',
-  against: ColorToken,
-  num?: number
-): ColorToken | Array<ColorToken> | Map<any, ColorToken>;
 
 export {
   brighten,
   darken,
-  getNearestColor,
-  colorDeficiency,
   isAchromatic,
   alpha,
-  getFarthestLightness,
-  getNearestLightness,
   overtone,
-  getFarthestChroma,
-  getNearestChroma,
   setChannel,
   setLuminance,
   getChannel,
   getLuminance,
   getContrast,
-  getFarthestContrast,
-  getNearestContrast,
   isCool,
   isWarm,
   getHueFamily,
-  getComplimentaryHue,
-  getFarthestHue,
-  getNearestHue,
-  getFarthestChromaFrom,
-  getFarthestHueFrom,
-  getFarthestLightnessFrom,
-  getNearestChromaFrom,
-  getNearestHueFrom,
-  getNearestLightnessFrom,
-  getFarthestLuminanceFrom,
-  getNearestLuminanceFrom,
-  getMeanChroma,
-  getMeanContrast,
-  getMeanDistance,
-  getMeanHue,
-  getMeanLightness,
-  getMeanLuminance
+  getComplimentaryHue
 };
