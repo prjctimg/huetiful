0e1485767d68405d2d01a64034e4aec64cee1afc
Dean Tarisai <prjctimg@outlook.com>
2024-03-20 18:24:18 +0200

    Updated the demo.json

--- docs/assets/js/meta.cjs
+++
@@ -1,7 +0,0 @@
-let x = '[`ColorToken`](types.md#colortoken)';
-
-x = x.replace(
-  new RegExp(/^([a-z]).md(#[a-z0-9])$/, 'gi'),
-  `https://huetiful-js.com/api/$1/index.html$2`
-);
-console.log(x);
