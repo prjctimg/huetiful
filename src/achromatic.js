0afcca22c499481ff55f381b3d883a5b4f45d8ae
xml-wizard <arcaneqoder@gmail.com>
2024-04-24 07:17:22 +0200

    ✨ feat(achromatic.js): achromatic now checks if a color is gray by comparing rgb channel values

--- src/achromatic.js
+++ src/achromatic.js
@@ -1,17 +1,20 @@
+// @ts-nocheck
 /**
  * @typedef { import('../types/types.js').ColorToken} ColorToken
  */
 
-import { mlchn, mcchn } from './fp/index.js';
+import { mlchn, mcchn, eq } from './fp/index.js';
 import { get } from './get.js';
+import { token } from './token.js';
 
 /**
  * Checks if a color token is achromatic (without hue or simply grayscale).
  * 
  * A color token is considered achromatic or gray if:
  * 
- * * It has a falsy chroma/saturation channel when its channel values are computed in a hue based colorspace because the hue channel is hue dependant.
- * * It has a falsy hue channel (usually happens if you use a custom interpolation method other than interpolatorLinear and one of the hue channels in the interpolation has a falsy channel) which makes the hue `NaN`
+ * * It has a falsy chroma/saturation channel when its channel values are computed in a hue based colorspace because the hue channel depends on the chroma channel for the final color to be non-gray (or colorful).
+ * * It has a falsy hue channel (usually happens if you use a custom interpolation method other than interpolatorLinear and one of the hue channels in the interpolation has a falsy channel) which makes the hue `NaN`.
+ * * All its `[r,g,b]` channels have equal values since grays are a result of interpolating black (`['rgb',0,0,0]`) and white (`['rgb',1,1,1]`). Therefore black and white return `false` because they're not grays.
  * @param {ColorToken} color The color token to test if it is achromatic or not.
  * @returns {boolean} True if the color token is achromatic else false.
  * @example
@@ -59,13 +62,17 @@ function achromatic(color) {
   // If a color has no lightness then it has no hue so its technically not achromatic since white and black are not grayscale
   var o = {
     l: get(`${mlchn('jch')}`)(color),
-    c: get(`${mcchn('jch')}`)(color)
+    c: get(`${mcchn('jch')}`)(color),
+    p: token('array', { targetMode: 'rgb', omitMode: true })(color)
   };
+  var { l, c, p } = o;
 
+  q = eq(p[0], p[1]) && eq(p[0], p[2]) && eq(p[2], p[1]);
   // Check if the saturation channel is zero or falsy for color spaces with saturation/chroma channel
-  return o['c'] &&
-    o['l'] !==
-      (false || NaN || undefined || void 0 || 0 || Infinity || -Infinity)
+
+  return c &&
+    l !== (false || NaN || undefined || void 0 || 0 || Infinity || -Infinity) &&
+    !q
     ? false
     : true;
 }
