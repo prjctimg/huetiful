ede56ce4e5ac38bad0d5bd26088b99e3a6e904c0
xml-wizard <arcaneqoder@gmail.com>
2024-04-24 15:31:02 +0200

    🧹 chore(family.js): fixed the documentation

--- src/family.js
+++ src/family.js
@@ -1,5 +1,5 @@
 /**
- * @typedef { import('../types/types.js').TailwindColorFamilies} TailwindColorFamilies
+ * @typedef { import('../types/types.js').HueFamily}HueFamily
  * @typedef { import('../types/types.js').ColorToken} ColorToken
  */
 import hue from './maps/hue.js';
@@ -7,9 +7,11 @@ import { customConcat, lt, max, entries } from './fp/index.js';
 import { get } from './get.js';
 
 /**
- * Gets the hue family which a color belongs to with the overtone included (if it has one.). For achromatic colors it returns the string "gray".
+ * Gets the hue family which a color belongs to with the overtone included (if it has one.).
+ * 
+ * For example `'red'` or `'blue-green'`. If the color is achromatic it returns the string `'gray'`.
  * @param {ColorToken} color The color to query its shade or hue family.
- * @returns {TailwindColorFamilies} The name of the hue family for example `red` or `blue-green`.
+ * @returns {HueFamily}
  * @example
  *
  * import { family } from 'huetiful-js'
