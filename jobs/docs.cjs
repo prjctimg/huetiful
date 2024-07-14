38aecc5bf7f8e371364a70763201c7146cb78f5b
Dean Tarisai <arcaneqoder@gmail.com>
2024-07-14 22:18:03 +0000

    📝 docs(docs.cjs): created script to add frontmatter to docs

--- jobs/docs.cjs
+++ jobs/docs.cjs
@@ -1,4 +1,5 @@
-var fs = require("fs");
+var fs = require("fs"),
+  summ = require("../www/data/summary.json");
 
 function page(title, date, lastmod, summary, content) {
   return `---
@@ -11,15 +12,40 @@ summary: ${summary}
 ${content}
 `;
 }
-var pathToMD = `./temp`;
+var pathToMD = `./.temp/`;
 
-for (const doc of fs
-  .readdirSync(pathToMD, "utf-8")
-  .map((x) => x.split(".mdx"))) {
-  const meta = fs.statSync(`../src/${doc}/index.js`);
+for (const doc of [
+  "accessibility",
+  "collection",
+  "generators",
+  "palettes",
+  "wrappers",
+  "utilities",
+]) {
+  const meta = fs.statSync(`./src/${doc}/index.js`);
   //   for each doc append its data to the template and write to the www/ dir
   // after that delete the temp folder and regenerate  the toc component using data from navigation.json
 
-  .fs
-    .writeFileSync(`../www/data/blog/${doc}`, page(doc,meta.birthtime,meta.mtime,));
+  fs.writeFileSync(
+    `./www/data/blog/${doc}.mdx`,
+    page(
+      doc,
+      meta.birthtime.toLocaleDateString("en-US", {
+        day: "2-digit",
+        month: "short",
+        year: "numeric",
+      }),
+      meta.mtime.toLocaleDateString("en-US", {
+        dayPeriod: "short",
+        day: "2-digit",
+        month: "short",
+
+        weekday: "short",
+      }),
+      summ[doc],
+      fs.readFileSync(pathToMD + doc + ".mdx", "utf8")
+    )
+  );
 }
+
+fs.rmdirSync("./.temp", { force: true, recursive: true });
