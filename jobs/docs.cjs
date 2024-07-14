bbd7cf132599425309608c4d47779f4e7bf3e422
Dean Tarisai <arcaneqoder@gmail.com>
2024-07-14 20:26:52 +0000

    📝 docs(summary.json): created a summary.json file and currently working on the frontmatter

--- jobs/docs.cjs
+++ jobs/docs.cjs
@@ -13,7 +13,13 @@ ${content}
 }
 var pathToMD = `./temp`;
 
-for (const doc of fs.readdirSync(pathToMD, "utf-8")) {
+for (const doc of fs
+  .readdirSync(pathToMD, "utf-8")
+  .map((x) => x.split(".mdx"))) {
+  const meta = fs.statSync(`../src/${doc}/index.js`);
   //   for each doc append its data to the template and write to the www/ dir
   // after that delete the temp folder and regenerate  the toc component using data from navigation.json
+
+  .fs
+    .writeFileSync(`../www/data/blog/${doc}`, page(doc,meta.birthtime,meta.mtime,));
 }
