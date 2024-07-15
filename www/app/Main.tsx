7f73a5222e8a9f5564325d9966ea2a0269a86ec4
Dean Tarisai <arcaneqoder@gmail.com>
2024-07-15 16:26:11 +0000

    docs: fixed homepage  at last

--- www/app/Main.tsx
+++ www/app/Main.tsx
@@ -1,98 +1,24 @@
+// @ts-nocheck
+
 import Link from "@/components/Link";
 import Tag from "@/components/Tag";
 import siteMetadata from "@/data/siteMetadata";
 import { formatDate } from "pliny/utils/formatDate";
 import { MDXLayoutRenderer } from "pliny/mdx-components";
 import { allBlogs } from "contentlayer/generated";
+
 const MAX_DISPLAY = 10;
 
 export default function Home({ posts }) {
-  const pageContent = allBlogs["blog__indexMdx"];
+  const pageContent = allBlogs.find((p) => p.slug === "blog");
 
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-          <MDXLayoutRenderer code={pageContent.body.raw} />
-        
+          <MDXLayoutRenderer code={pageContent.body.code} />
         </div>
-      
       </div>
-
-      
     </>
   );
 }
-
-
-// {
-//   posts.length > MAX_DISPLAY && (
-//     <div className="flex justify-end text-base font-medium leading-6">
-//       <Link
-//         href="/blog"
-//         className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-//         aria-label="All posts"
-//       >
-//         See more&rarr;
-//       </Link>
-//     </div>
-//   );
-// }
-
-
-
-  /*{
-    <ul className="divide-y divide-gray-200 dark:divide-gray-700">
-          {!posts.length && "No posts found."}
-          {posts.slice(0, MAX_DISPLAY).map((post) => {
-            const { slug, date, title, summary, tags } = post;
-            return (
-              <li key={slug} className="py-12">
-                <article>
-                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                    <dl>
-                      <dt className="sr-only">Published on</dt>
-                      <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                        <time dateTime={date}>
-                          {formatDate(date, siteMetadata.locale)}
-                        </time>
-                      </dd>
-                    </dl>
-                    <div className="space-y-5 xl:col-span-3">
-                      <div className="space-y-6">
-                        <div>
-                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
-                            <Link
-                              href={`/blog/${slug}`}
-                              className="first-letter:uppercase text-gray-900 dark:text-gray-100"
-                            >
-                              {title}
-                            </Link>
-                          </h2>
-                          <div className="flex flex-wrap">
-                            {tags.map((tag) => (
-                              <Tag key={tag} text={tag} />
-                            ))}
-                          </div>
-                        </div>
-                        <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-                          {summary}
-                        </div>
-                      </div>
-                      <div className="text-base font-medium leading-6">
-                        <Link
-                          href={`/blog/${slug}`}
-                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                          aria-label={`Read more: "${title}"`}
-                        >
-                          Read more &rarr;
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-                </article>
-              </li>
-            );
-          })}
-        </ul> */
-  }
