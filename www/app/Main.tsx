072b60bb9923d68ec8041454229e38cefc7af085
Dean Tarisai <arcaneqoder@gmail.com>
2024-07-15 12:08:58 +0000

    docs: fixed homepage

--- www/app/Main.tsx
+++ www/app/Main.tsx
@@ -2,7 +2,7 @@ import Link from "@/components/Link";
 import Tag from "@/components/Tag";
 import siteMetadata from "@/data/siteMetadata";
 import { formatDate } from "pliny/utils/formatDate";
-
+import { MDXLayoutRenderer } from "pliny/mdx-components";
 const MAX_DISPLAY = 10;
 
 export default function Home({ posts }) {
@@ -10,11 +10,12 @@ export default function Home({ posts }) {
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+          <img src="/static/images/logo.svg" alt="" />
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
           </p>
           <h2 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Latest
+            Modules
           </h2>
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
@@ -71,6 +72,7 @@ export default function Home({ posts }) {
           })}
         </ul>
       </div>
+
       {posts.length > MAX_DISPLAY && (
         <div className="flex justify-end text-base font-medium leading-6">
           <Link
