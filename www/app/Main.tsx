f1d491b4f77a0bb03be7070138ee9d29a10e97ae
Dean Tarisai <arcaneqoder@gmail.com>
2024-07-15 14:21:56 +0000

    docs: fixed homepage

--- www/app/Main.tsx
+++ www/app/Main.tsx
@@ -3,22 +3,46 @@ import Tag from "@/components/Tag";
 import siteMetadata from "@/data/siteMetadata";
 import { formatDate } from "pliny/utils/formatDate";
 import { MDXLayoutRenderer } from "pliny/mdx-components";
+import { allBlogs } from "contentlayer/generated";
 const MAX_DISPLAY = 10;
 
 export default function Home({ posts }) {
+  const pageContent = allBlogs["blog__indexMdx"];
+
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-          <img src="/static/images/logo.svg" alt="" />
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            {siteMetadata.description}
-          </p>
-          <h2 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Modules
-          </h2>
+          <MDXLayoutRenderer code={pageContent.body.raw} />
+        
         </div>
-        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+      
+      </div>
+
+      
+    </>
+  );
+}
+
+
+// {
+//   posts.length > MAX_DISPLAY && (
+//     <div className="flex justify-end text-base font-medium leading-6">
+//       <Link
+//         href="/blog"
+//         className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+//         aria-label="All posts"
+//       >
+//         See more&rarr;
+//       </Link>
+//     </div>
+//   );
+// }
+
+
+
+  /*{
+    <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {!posts.length && "No posts found."}
           {posts.slice(0, MAX_DISPLAY).map((post) => {
             const { slug, date, title, summary, tags } = post;
@@ -70,20 +94,5 @@ export default function Home({ posts }) {
               </li>
             );
           })}
-        </ul>
-      </div>
-
-      {posts.length > MAX_DISPLAY && (
-        <div className="flex justify-end text-base font-medium leading-6">
-          <Link
-            href="/blog"
-            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-            aria-label="All posts"
-          >
-            See more&rarr;
-          </Link>
-        </div>
-      )}
-    </>
-  );
-}
+        </ul> */
+  }
