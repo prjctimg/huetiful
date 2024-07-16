a6a0d86364b9e44891d0cf634ebaa8431977a1df
Dean Tarisai <arcaneqoder@gmail.com>
2024-07-16 11:41:06 +0000

    📝 docs(docs.cjs): fixed summary not rendering

--- jobs/docs.cjs
+++ jobs/docs.cjs
@@ -1,12 +1,25 @@
 var fs = require("fs"),
-  summ = require("../www/data/summary.js");
+  summ = {
+    accessibility:
+      "Functions to help ensure your palettes and color scales are accessible for a wider audience.",
+    wrappers:
+      'Wrapper functions for creating "read-manipulate-output " chains (via method chaining) for a more concise syntax.',
+    generators:
+      "Utilites for creating palettes by mapping strategic values to target channels on color tokens.",
+    palettes:
+      "Precomputed color maps from Tailwind and ColorBrewer exposed as wrapper functions making it easy to get your designs started.",
+    utilities:
+      "Functions for general purpose set/get snd conversion operations on color tokens.",
+    collection:
+      "Utilities for querying statistics,soring,filtering and distributing properties on collections of colors. ",
+  };
 
 function page(title, date, lastmod, summary, content, canonicalUrl) {
   return `---
 title: ${title}
 date: ${date}
 lastmod: ${lastmod}
-summary: ${summary}
+${summary ? `summary: ${summary}` : ""}
 canonicalUrl: ${canonicalUrl}
 ---
 \n
--- www/app/Main.tsx
+++ www/app/Main.tsx
@@ -19,76 +19,8 @@ export default function Home({ posts }) {
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
           </p>
-          <h2 className="text-xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Modules
-          </h2>
         </div>
-        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
-          {!posts.length && "No posts found."}
-          {posts.slice(0, MAX_DISPLAY).map((post) => {
-            const { slug, date, title, summary, tags } = post;
-            return (
-              <li key={slug} className="py-12">
-                <article>
-                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                    <dl>
-                      <dt className="sr-only">Published on</dt>
-                      <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                        <time dateTime={date}>
-                          {formatDate(date, siteMetadata.locale)}
-                        </time>
-                      </dd>
-                    </dl>
-                    <div className="space-y-5 xl:col-span-3">
-                      <div className="space-y-6">
-                        <div>
-                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
-                            <Link
-                              href={`/blog/${slug}`}
-                              className="first-letter:uppercase text-gray-900 dark:text-gray-100"
-                            >
-                              {toUppercase(title)}
-                            </Link>
-                          </h2>
-                          <div className="flex flex-wrap">
-                            {tags.map((tag) => (
-                              <Tag key={tag} text={tag} />
-                            ))}
-                          </div>
-                        </div>
-                        <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-                          {summary}
-                        </div>
-                      </div>
-                      <div className="text-base font-medium leading-6">
-                        <Link
-                          href={`/blog/${slug}`}
-                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                          aria-label={`Read more: "${title}"`}
-                        >
-                          Read more &rarr;
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-                </article>
-              </li>
-            );
-          })}
-        </ul>
       </div>
-
-      {posts.length > MAX_DISPLAY && (
-        <div className="flex justify-end text-base font-medium leading-6">
-          <Link
-            href="/blog"
-            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-            aria-label="All posts"
-          >
-            See more&rarr;
-          </Link>
-        </div>
-      )}
     </>
   );
 }
